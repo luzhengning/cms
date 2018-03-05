2f5489514799586c991a09cc7c5ebc8865373046
luzhengning <1738515000@qq.com>
2018-03-05 18:03:25 +0800

    1.AAAA灾后重建

--- src/main/java/com/geek/cms/utils/Converts.java
+++ src/main/java/com/geek/cms/utils/Converts.java
@@ -10,10 +10,7 @@ public class Converts {
 	 * @return
 	 */
 	public static List<Integer> ArrayToListInteger(String[] strs){
-<<<<<<< HEAD
 		if(strs==null)return null;
-=======
->>>>>>> refs/remotes/origin/master
 		List<Integer> list=new ArrayList<Integer>();
 		for(int i=0;i<strs.length;i++) {
 			if(strs[i]==null||strs[i].trim().length()==0)continue;
